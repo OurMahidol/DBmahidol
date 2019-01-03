8244653e1ff6b20a765b54e5a6a7d1d6e3d834dc
OurMahidol <46133632+OurMahidol@users.noreply.github.com>
2019-01-03 20:17:57 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.get("/", (req, res) => {
 
     MongoClient.connect(url, function(err, db) {
         if (err) throw err;
-        var dbo = db.db("mydb");
+        var dbo = db.db("heroku_gfwhqm01");
         dbo.createCollection("customers", function(err, res) {
           if (err) throw err;
           console.log("Collection created!");
