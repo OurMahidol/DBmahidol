44c7536daa0653c5cba157d55146d5a4b75d8e27
OurMahidol <46133632+OurMahidol@users.noreply.github.com>
2019-01-03 20:00:46 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -14,15 +14,31 @@ var port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
 
-  MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  var dbo = db.db("mydb");
-  dbo.createCollection("customers", function(err, res) {
-    if (err) throw err;
-    console.log("Collection created!");
-    db.close();
-  });
-}); 
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("mydb");
+        dbo.createCollection("customers", function(err, res) {
+          if (err) throw err;
+          console.log("Collection created!");
+          db.close();
+        });
+      }); 
+/*var responseObject = {
+
+    status: true,
+
+    data: { text: "hello" }
+
+}
+
+res.json(responseObject);*/
 
 
 });
+
+
+app.listen(port, () => {
+
+    console.log("application is listening on:", port);
+ 
+ });
