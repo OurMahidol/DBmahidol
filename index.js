c02f063a82ac92604a4f512e0ec214796b599687
OurMahidol <46133632+OurMahidol@users.noreply.github.com>
2019-01-03 19:42:57 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://heroku_gfwhqm01:arjf5iak66nl3usm0v1vaqmh3r@ds251022.mlab.com:51022/heroku_gfwhqm01";
 
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  var dbo = db.db("mydb");
-  dbo.createCollection("customers", function(err, res) {
-    if (err) throw err;
-    console.log("Collection created!");
-    db.close();
-  });
-}); 
+
+var express = require("express");
+
+var app = express();
+
+
+
+var port = process.env.PORT || 5000;
+
+
+
+app.get("/", (req, res) => {
+
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("mydb");
+        dbo.createCollection("customers", function(err, res) {
+          if (err) throw err;
+          console.log("Collection created!");
+          db.close();
+        });
+      }); 
+      
+
+});
+
+app.listen(port, () => {
+
+    console.log("application is listening on:", port);
+ 
+ });
